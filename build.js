fddb83ffbac0a612741f7ef6efac4af16171150b
Max Fechner <max.fechner@gmail.com>
2017-01-03 13:47:58 +0800

    ...

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 const PubLit = '../../PubLit/';
 const fs = require('fs');
+const LitMD = require(PubLit + 'LitMD');
 
 
 addStyle = (name) => {
@@ -21,7 +22,9 @@ mkdir = (dir) => {
 	}
 }
 
-buildLanguages = () => {
+
+
+buildLanguageList = () => {
 	console.log('buildLanguages');
 	var HTML = '';
 	HTML += addStyle('base');
@@ -31,10 +34,10 @@ buildLanguages = () => {
 	HTML += '<table>'
 	fs.readdirSync(PubLit).forEach((item) => {
 		if (item[0] == '.') return;
-		if (item.substr(-3) == '.md') return;
+		if (item.includes('.')) return;
 		HTML += makeRow(item.substr(0, 2), '', item.substr(3));
 		// mkdir(item.substr(0, 2));
-		buildAuthors(item, item.substr(0, 2));
+		buildAuthorList(item, item.substr(0, 2));
 	});
 	HTML += '</table>';
 	HTML += '</main>';
@@ -42,7 +45,7 @@ buildLanguages = () => {
 }
 
 
-buildAuthors = (sourcePath, targetPath) => {
+buildAuthorList = (sourcePath, targetPath) => {
 	console.log('buildAuthors', sourcePath, targetPath);
 	mkdir(targetPath);
 	var HTML = '';
@@ -60,13 +63,14 @@ buildAuthors = (sourcePath, targetPath) => {
 			var url = author.substr(5).split(' ').join('');
 			HTML += makeRow(url, author.substr(0, 4), author.substr(5));
 			// mkdir(targetPath + '/' + url);
-			buildBooks(sourcePath + '/' + type + '/' + author, targetPath + '/' + url);
+			buildAuthorPage(sourcePath + '/' + type + '/' + author, targetPath + '/' + url);
 		});
 		HTML += '</table>\n\n\n';
 	});
 	HTML += '</main>';
 	fs.writeFileSync(targetPath + '/index.html', HTML);
 }
+
 
 
 addBiography = (sourcePath, targetPath) => {
@@ -82,6 +86,7 @@ addBiography = (sourcePath, targetPath) => {
 	return HTML;
 	// return '<p>' + bio.split('\n\n').join('</p>\n\n<p>') + '</p>';
 }
+
 addCV = (sourcePath) => {
 	if (!fs.existsSync(PubLit + sourcePath + '/cv.txt')) return '';
 	var cv = fs.readFileSync(PubLit + sourcePath + '/cv.txt', 'utf-8');
@@ -96,6 +101,7 @@ addCV = (sourcePath) => {
 	HTML += `</table>\n`;
 	return HTML;
 }
+
 addPicture = (sourcePath, targetPath) => {
 	if (!fs.existsSync(PubLit + sourcePath + '/pic.jpg')) return '';
 
@@ -111,32 +117,64 @@ addPicture = (sourcePath, targetPath) => {
 	return `<img src='pic.jpg'/>`;
 }
 
-buildBooks = (sourcePath, targetPath) => {
-	console.log('buildBooks', sourcePath, targetPath);
-	var author = sourcePath.split('/').slice(-1)[0].substr(5);
-	mkdir(targetPath);
+addBooks = (sourcePath, targetPath) => {
 	var HTML = '';
-	HTML += `<title>${author}</title>\n`;
-	HTML += addStyle('base');
-	HTML += addStyle('books');
-	HTML += '<main>\n';
-	HTML += `<h1>${author}</h1>`;
-	HTML += addBiography(sourcePath, targetPath);
-	HTML += addCV(sourcePath);
 	fs.readdirSync(PubLit + sourcePath).forEach((genre) => {
 		if (genre.includes('.')) return;
 		HTML += `<h2>${genre}</h2>\n\n`;
 		HTML += '<table>\n'
 		fs.readdirSync(PubLit + sourcePath + '/' + genre).forEach((book) => {
 			if (book[0] == '.') return;
-			var url = book.substr(5).split(' ').join('').replace('.md', '');
-			HTML += makeRow(url, book.substr(0, 4), book.substr(5).replace('.md', ''));
+			var title = book.substr(5).replace('.md', '');
+			var url = title.split(' ').join('');
+			HTML += makeRow(url, book.substr(0, 4), title);
+			buildBookPage(sourcePath + '/' + genre + '/' + book, targetPath + '/' + url);
 		});
 		HTML += '</table>\n\n\n';
 	});
+	return HTML;
+}
+
+buildAuthorPage = (sourcePath, targetPath) => {
+	console.log('buildBooks', sourcePath, targetPath);
+	var author = sourcePath.split('/').slice(-1)[0].substr(5);
+	mkdir(targetPath);
+	var HTML = '';
+	HTML += `<title>${author}</title>\n`;
+	HTML += addStyle('base');
+	HTML += addStyle('author');
+	HTML += '<main>\n';
+	HTML += `<h1>${author}</h1>`;
+	HTML += addBiography(sourcePath, targetPath);
+	HTML += addCV(sourcePath);
+	HTML += addBooks(sourcePath, targetPath);
 	HTML += '</main>\n';
 	fs.writeFileSync(targetPath + '/index.html', HTML);
 }
 
 
-buildLanguages();
+buildBookPage = (sourcePath, targetPath) => {
+	console.log('buildBookPage', sourcePath, targetPath);
+	mkdir(targetPath);
+	var parts = sourcePath.split('/');
+	var author = parts[2].substr(5);
+	var title = parts[4].substr(5).replace('.md', '');
+	var HTML = '';
+	HTML += `<title>${author}</title>\n`;
+	HTML += addStyle('base');
+	HTML += addStyle('book');
+	// HTML += addStyle('books');
+	HTML += '<main>\n';
+	HTML += `<h1>${title}</h1>`;
+	HTML += `<h2>${author}</h1>`;
+	var content = fs.readFileSync(PubLit + sourcePath, 'utf-8');
+	HTML += `<article>${LitMD.LitMD(content)}</article>`;
+	HTML += '</main>\n';
+	fs.writeFileSync(targetPath + '/index.html', HTML);
+
+}
+
+buildLanguageList();
+
+
+// more
